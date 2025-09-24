806b362720f99792f5d8b3b02b2cb89d1d9d1f95
PedroAnt628 <pemorecarr@gmail.com>
2025-09-24 14:21:44 +0200

    primera clase de fundamentos

--- fundamentos JAVA/src/App.java
+++ fundamentos JAVA/src/App.java
@@ -1,5 +1,42 @@
 public class App {
+    public static boolean logico=true;
+    final static double PI=3.14345;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    //    System.out.println("Hello, World!");
+    // String nombre="soy pedro";
+    // int edad=19;
+    // double salario=16.5464564;
+    
+    //int numero entero
+    //double numero real/decimal
+    //string cadena de texto
+    //char caracter
+    //boolean true or false
+
+    //int cuenta= 1/3;
+    //double cuenta2=1/3.0;
+    //System.out.println(cuenta);
+    //System.out.println(cuenta2);
+    //double a=(int)2.6;
+    //System.out.println(a);
+
+    System.out.println(logico);
+    System.out.println(PI);
+    
+    int b='b'; //UN caracter se guarda en un enteor segun ASCII
+    char c=98;
+    System.out.println(b+" "+c);
+
+    }
+    /**
+     * Programa para impimir texto
+     * @author Pedro Antonio
+     * @param texto a imprimir 
+     * @version 1.0
+     */
+
+    public static void imprimirTexto(String texto){
+        boolean logico=false;
     }
 }
