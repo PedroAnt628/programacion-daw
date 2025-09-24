54f961ff2964679020811285fbbef717c88d2537
PedroAnt628 <pemorecarr@gmail.com>
2025-09-24 21:21:52 +0200

    mi primera clase de fundamentos

--- fundamentos JAVA/src/App.java
+++ fundamentos JAVA/src/App.java
@@ -3,23 +3,23 @@ public class App {
     final static double PI=3.14345;
 
     public static void main(String[] args) throws Exception {
-    //    System.out.println("Hello, World!");
-    // String nombre="soy pedro";
-    // int edad=19;
-    // double salario=16.5464564;
+       System.out.println("Hello, World!");
+    String nombre="soy pedro";
+    int edad=19;
+    double salario=16.5464564;
     
-    //int numero entero
-    //double numero real/decimal
-    //string cadena de texto
-    //char caracter
-    //boolean true or false
+    // int numero entero
+    // double numero real/decimal
+    // string cadena de texto
+    // char caracter
+    // boolean true or false
 
-    //int cuenta= 1/3;
-    //double cuenta2=1/3.0;
-    //System.out.println(cuenta);
-    //System.out.println(cuenta2);
-    //double a=(int)2.6;
-    //System.out.println(a);
+    int cuenta= 1/3;
+    double cuenta2=1/3.0;
+    System.out.println(cuenta);
+    System.out.println(cuenta2);
+    double a=(int)2.6;
+    System.out.println(a);
 
     System.out.println(logico);
     System.out.println(PI);
@@ -40,6 +40,6 @@ public class App {
      */
 
     public static void imprimirTexto(String texto){
-        boolean logico=false;
+        int logico=3;
     }
 }
