23a8feeec601031f6cf04836d6e89c2fa0d129a5
PedroAnt628 <pemorecarr@gmail.com>
2025-10-06 19:58:34 +0200

    actividades

--- actividad/src/actividades4.java
+++ actividad/src/actividades4.java
@@ -19,10 +19,27 @@ public class actividades4 {
     
 //     }
 //     public static void imprimeCilindro(double altura, double radio){
-//         System.out.println(altura*radio*PI);
-//         System.out.println(altura*radio*PI);
+//         System.out.println(2*PI*(altura+radio));
+//         System.out.println(PI*(radio*radio)*altura);
 //     }
 
-
-
+        //Crear las funciones esPar, esDivisible3 y un
+        //procedimiento para imprimir los números pares divisibles por 3 que
+        //hay entre 1 y 20
+        public static boolean esPar(int numero){
+            return numero%2==0;
+        }
+        public static boolean esDivisible3(int numero){
+            return numero%3==0;
+        }
+        public static void main(String[] args) {
+            
+        for (int i=1;i<=20;i++){
+            if (esPar(i)&& esDivisible3(i)){
+                System.out.println(i);
+            }
+        }
+        
 }
+}
+
