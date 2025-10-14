21867740d03d2dbc7e18972ce7976bb9835c7e35
PedroAnt628 <pemorecarr@gmail.com>
2025-10-15 00:27:49 +0200

    arrays

--- arrays_java/src/App.java
+++ arrays_java/src/App.java
@@ -80,41 +80,78 @@ public class App {
         //     System.out.println();
         // }
 
+
+
+
+
+
+        //Ejercicios
+
         //Realiza una función que busca un número en un array e indica dónde está
-        int[] vector={1,2,3,4,5,6,7,8,9,10};
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce un numero a buscar");
-        int num=teclado.nextInt();
-        boolean encontrado=false;
-        for(int i=0;i<vector.length && !encontrado;i++) {
-            if(vector[i]==num) {
-                System.out.println("El numero "+num+" se encuentra en la posicion "+i);
-                encontrado=true;
-            }
-        }
+        // int[] buscar={1,2,3,4,5,6,7,8,9,10};
+        // Scanner teclado = new Scanner(System.in);
+        // System.out.println("Introduce un numero a buscar");
+        // int num=teclado.nextInt();
+        // for(int i=0;i<buscar.length;i++) {
+        //     if(buscar[i]==num) {
+        //         System.out.println("El numero "+num+" se encuentra en la posicion "+i);
+        //     }
+        // }
         
-        //Realiza un programa que muestra tu horario y qué clases tienes hoy
-        String[][] horario={
-            {"Lunes", "Prog", "LM", "BD"},
-            {"Martes", "Prog", "Entor", "BD"},
-            {"Miercoles", "Prog", "Prog", "Entor"},
-            {"Jueves", "Prog", "BD", "BD"},
-            {"Viernes", "Prog", "LM", "LM"}
-        };
-        System.out.println("Introduce un dia de la semana");
-        String dia=teclado.next();
-        boolean diaEncontrado=false;
-        for(int i=0;i<horario.length && !diaEncontrado;i++) {
-            if(horario[i][0].equalsIgnoreCase(dia)) {
-                System.out.print("El dia "+dia+" tienes las siguientes clases: ");
-                for(int j=1;j<horario[i].length;j++) {
-                    System.out.print(horario[i][j]+" ");
-                }
-                diaEncontrado=true;
+        // //Realiza un programa que muestra tu horario y qué clases tienes hoy
+        // String[][] horario={
+        //     {"Lunes", "Prog", "LM", "BD"},
+        //     {"Martes", "Prog", "Entor", "BD"},
+        //     {"Miercoles", "Prog", "Prog", "Entor"},
+        //     {"Jueves", "Prog", "BD", "BD"},
+        //     {"Viernes", "Prog", "LM", "LM"}
+        // };
+        // System.out.println("Introduce un dia de la semana");
+        // String dia=teclado.next();
+        // for(int i=0;i<horario.length;i++) {
+        //     if(horario[i][0].equalsIgnoreCase(dia)) {
+        //         System.out.print("El "+dia+" tienes: ");
+        //         for(int j=1;j<horario[i].length;j++) {
+        //             System.out.print(horario[i][j]+" ");
+        //         }
+        //     }
+        // }
+
+        //Realiza un programa que inserta valores en una matriz dinámica y devuelva la
+        //suma de los valores de cada fila y columna
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el numero de filas");
+        int filas=teclado.nextInt();
+        System.out.println("Introduce el numero de columnas");
+        int columnas=teclado.nextInt();
+
+        int [][] matriz=new int[filas][columnas];
+        for(int i=0;i<filas;i++) {
+            for(int j=0;j<columnas;j++) {
+                System.out.println("Introduce el valor de la posicion "+i+","+j);
+                matriz[i][j]=teclado.nextInt();
+            }
+        }
+        int[] sumaFilas=new int[filas];
+        int[] sumaColumnas=new int[columnas];
+
+        for(int i=0;i<filas;i++) {
+            for(int j=0;j<columnas;j++) {
+                sumaFilas[i]+=matriz[i][j];
+                sumaColumnas[j]+=matriz[i][j];
             }
         }
 
+        System.out.println("Suma de filas:");
+        for(int i=0;i<filas;i++) {
+            System.out.println("Fila "+i+": "+sumaFilas[i]);
+        }
 
-        // teclado.close();
+        System.out.println("Suma de columnas:");
+        for(int j=0;j<columnas;j++) {
+            System.out.println("Columna "+j+": "+sumaColumnas[j]);
+        }
+
+        teclado.close();
     }
 }
