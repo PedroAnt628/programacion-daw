f4bf8a765a7be772332d27b777cd1bbfb4c00498
PedroAnt628 <pemorecarr@gmail.com>
2025-10-23 22:13:14 +0200

    practica

--- TicTacToe/src/App.java
+++ TicTacToe/src/App.java
@@ -1,43 +1,49 @@
 import java.util.Scanner;
 
 public class App {
-    //Crear matriz
-        static int [][] matriz={{0,0,0},{0,0,0},{0,0,0}};
-        //Variable de turno, por defecto empieza el jugador 1
+    //Crear la matriz 
+        static int[][] matriz={{0,0,0},{0,0,0},{0,0,0}};
+        //Variable de turno, que por defecto empieza el jugador 1
         static boolean turno=true;
         final static int JUEGO1=1;
         final static int JUEGO2=2;
     public static void main(String[] args) throws Exception {
         Scanner teclado=new Scanner(System.in);
         int posicion;
+        //Mostrar situación inicial
+        imprimeMatriz();
 
-        // for (int i=0;i<9;i++){
-        //     System.out.println("Ingrese posicion entre el 1 y 9: ");
+        for(int i=0;i<9;i++){
+            do{
+                do{
+                    System.out.print("Introduce coordenada del 1 al 9: ");
+                    posicion=teclado.nextInt();
+                }while(posicion<0 || posicion>9);
+            }while(!valida(posicion));
+            jugada(turno,posicion);
+            if(comprobarGanador(turno)){
+                if(turno==true) System.out.println("El juagador1 ha ganado");
+                else {System.out.println("El juagador2 ha ganado");}
+                break;
+            }
+            imprimeMatriz();
+            turno=!turno;
+        }
+        if(!comprobarGanador(turno) && !comprobarGanador(!turno))//Si llegas aquí comprobamos si no ha ganado nadie
+            System.out.println("Hay un empate");
+
+        // do{
+        //     System.out.print("Introduce coordenada del 1 al 9: ");
         //     posicion=teclado.nextInt();
-        //     jugada(matriz, turno, posicion);
-        //     if (comprobarGanador(matriz, turno)){
-        //         if (turno==true) System.out.println("El jugador 1 ha ganado");
-        //         else System.out.println("El jugador 2 ha ganado");
-        //         break;
-        //     }
-        //     turno = !turno;
-        //     imprimirMatriz(matriz);
-        // }
-        do {
-            System.out.println("Ingrese posicion entre el 1 y 9: ");
-            posicion=teclado.nextInt();
-            jugada(matriz, turno, posicion);
-            turno = !turno;
-            imprimirMatriz();
-
-        } while (!comprobarGanador(matriz, turno));
-        if (turno==true) System.out.println("El jugador 1 ha ganado");
-        else System.out.println("El jugador 2 ha ganado");
-
-        teclado.close();
-
+        //     jugada(turno,posicion);
+        //     imprimeMatriz();
+        //     turno=!turno;
+        // }while(!comprobarGanador(!turno));
+        // if(turno==true) System.out.println("El juagador1 ha ganado");
+        // else {System.out.println("El juagador2 ha ganado");}
+        
     }
-    public static void imprimirMatriz(){
+    public static void imprimeMatriz(){
         for(int i=0;i<matriz.length;i++){
             for(int j=0;j<matriz[i].length;j++){
                 System.out.print(matriz[i][j]+" ");
@@ -45,80 +51,100 @@ public class App {
             System.out.println();
         }
     }
-    public static void jugada(int[][]matriz, boolean turno, int posicion){
+    public static boolean valida(int posicion){
+        boolean control=true;
+        switch(posicion){
+            case 1: 
+                if (matriz[0][0]!=0) control=false;
+                break;
+            case 2: 
+                if(matriz[0][1]!=0) control=false;
+                break;
+            case 3: 
+                if(matriz[0][2]!=0) control=false;
+                break;
+            case 4: 
+                if (matriz[1][0]!=0) control=false;
+                break;
+            case 5: 
+                if (matriz[1][1]!=0) control=false;
+                break;
+            case 6: 
+                if (matriz[1][2]!=0) control=false;
+                break;
+            case 7: 
+                if(matriz[2][0]!=0) control=false;
+                break;
+            case 8: 
+                if (matriz[02][1]!=0) control=false;
+                break;
+            case 9: 
+                if (matriz[2][2]!=0) control=false;
+                break;
+        }
+        return control;
+    }
+    public static void jugada(boolean turno,int posicion){
         int valor;
         if (turno==true) valor=JUEGO1;
-        else {valor=JUEGO2;}
-        switch (posicion) {
-            case 1:
+        else{ valor=JUEGO2;}
+        switch(posicion){
+            case 1: 
                 matriz[0][0]=valor;
                 break;
-            case 2:
+            case 2: 
                 matriz[0][1]=valor;
                 break;
-            case 3:
+            case 3: 
                 matriz[0][2]=valor;
                 break;
-            case 4:
+            case 4: 
                 matriz[1][0]=valor;
                 break;
-            case 5:
+            case 5: 
                 matriz[1][1]=valor;
                 break;
-            case 6:
+            case 6: 
                 matriz[1][2]=valor;
                 break;
-            case 7:
+            case 7: 
                 matriz[2][0]=valor;
                 break;
-            case 8:
-                matriz[2][1]=valor;
+            case 8: 
+                matriz[02][1]=valor;
                 break;
-            case 9:
+            case 9: 
                 matriz[2][2]=valor;
                 break;
-            
-            default:
-                System.out.println("Has introducido un valor incorrecto");
-                break;
         }
-
+    }
+    public static boolean comprobarGanador(boolean turno){
+        //Determinar el valor a buscar en la matriz
+        int valor;
+        if(turno==true) valor=JUEGO1;
+        else{valor=JUEGO2;}
+        //Gana en las filas? 
+        if(compruebaFila(0,valor) || compruebaFila(1,valor) || compruebaFila(2,valor)) return true;
+        else if (compruebaColumna(0,valor) || compruebaColumna(1,valor)|| compruebaColumna(2,valor)) return true; // Gana por columnas?
+        else if (compruebaDiagonal1(valor) || compruebaDiagonal2(valor)) return true; // Gana por diagonales?
+        else return false; //NO HAS GANADO
+    
+    }
+    public static boolean compruebaDiagonal1(int valor){
+        if(matriz[0][0]==valor && matriz[1][1]==valor && matriz[2][2]==valor) return true;
+        return false;
+    }
+    public static boolean compruebaDiagonal2(int valor){
+        if(matriz[0][2]==valor && matriz[1][1]==valor && matriz[2][0]==valor) return true;
+        return false;
     }
 
-    public static boolean comprobarGanador(int[][]matriz, boolean turno){
-    int valor;
-    if (turno==true) valor=JUEGO1;
-    else {valor=JUEGO2;}
-
-    if (compruebafila(0, valor) || compruebafila(1, valor) || compruebafila(2, valor)) return true;
-    else if (compruebacolumna(0, valor) || compruebacolumna(1, valor) || compruebacolumna(2, valor)) return true;
-    else if (compruebadiagonal(0, valor) || compruebadiagonal(1, valor)) return true;
-    return false;
+    public static boolean compruebaFila(int fila,int valor){
+        if(matriz[fila][0]==valor && matriz[fila][1]==valor && matriz[fila][2]==valor) return true;
+        return false;
     }
-    public static boolean compruebafila(int fila, int valor){
-        for (int j=0;j<3;j++){
-            if (matriz[fila][j]!=valor) return false;
-        }
-        return true;
+    public static boolean compruebaColumna(int columna,int valor){
+        if(matriz[0][columna]==valor && matriz[1][columna]==valor && matriz[2][columna]==valor) return true;
+        return false;
     }
-    public static boolean compruebacolumna(int columna, int valor){
-        for (int i=0;i<3;i++){
-            if (matriz[i][columna]!=valor) return false;
-        }
-        return true;
-    }
-    public static boolean compruebadiagonal(int diagonal, int valor){
-        if (diagonal==0){
-            for (int i=0;i<3;i++){
-                if (matriz[i][i]!=valor) return false;
-            }
-        } else {
-            for (int i=0;i<3;i++){
-                if (matriz[i][2-i]!=valor) return false;
-            }
-        }
-        return true;
-
-    }
-
 }
--- prueba.java
+++ prueba.java
@@ -1,5 +1,77 @@
+
+import java.util.Scanner;
+
 public class prueba{
     public static void main(String[] args){
-        System.out.println("Hola Mundo!!");
+        //Numeros aleatorios y funciones matematicas 
+
+        // int a=(int)(Math.random() * (10 - 1 + 1)) + 1;
+        // int b=(int)(Math.random() * (15 - 5 + 1)) + 5;
+        
+        // double cociente= (double) a / b;
+        // double media= (a + b)/2.0;
+        // double potencia= (Math.pow(a, b));
+        // double raizA= (Math.sqrt(a));
+        // double raizB= (Math.sqrt(b));
+
+        // System.out.println("Número A: " + a);
+        // System.out.println("Número B: " + b);
+        // System.out.println("Cociente: " + cociente);
+        // System.out.println("Media: " + media);
+        // System.out.println("Potencia: " + potencia);
+        // System.out.println("Raíz de A: " + raizA);
+        // System.out.println("Raíz de B: " + raizB);
+
+        // //For porq conozco los valores que pide
+        // int numero1=0;
+        // for (int i = 1; i <= 10; i++) {
+        //     numero1=i;
+        //     System.err.println(numero1);
+        // }
+
+        // //While porque no sabemos las veces que se va a hacer
+        // int numero2;
+        // Scanner teclado=new Scanner(System.in);
+        // System.out.println("Introduce numero entre 0 y 10");
+        // numero2=teclado.nextInt();
+        // while (numero2<0 || numero2>10){
+        //     System.out.println("Introduce numero entre 0 y 10");
+        //     numero2=teclado.nextInt();
+        // }
+        // System.err.println("El numero valido es: "+ numero2);
+
+
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.print(i * 3+" ");
+        // }
+
+        // int edad;
+        // int edadmax = Integer.MIN_VALUE;         
+        // Scanner teclado=new Scanner(System.in);
+        // do {
+
+        //     System.err.println("Introduce una edad o -1 para salir");
+        //     edad=teclado.nextInt();
+        //     if (edad>edadmax && edad !=-1){
+        //         edadmax=edad;
+        //     }
+
+        // } while (edad !=-1);
+        // System.out.println("La edad máxima introducida fue: " + edadmax);
+        
+
+        int numero3;
+        System.out.println("Introdude la tabla que quieres multiplicar");
+        Scanner teclado=new Scanner(System.in);
+        numero3=teclado.nextInt();
+        int resultado;
+        for (int i = 1; i <= 10; i++) {
+            resultado = i * numero3;
+            System.out.println(numero3 + " x " + i + " = " + resultado);
+
+
+            
+        }
+
     }
 }
