dd29294b7be624d2cdf7022a4bfed7d5dc4e0ce6
PedroAnt628 <pemorecarr@gmail.com>
2025-11-05 14:22:06 +0100

    ahorcado

--- actividad/src/releacion_unidad3.java
+++ actividad/src/releacion_unidad3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class releacion_unidad3 {
@@ -48,71 +50,292 @@ public class releacion_unidad3 {
 
 
     //Ejercicio 2
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+    // public static void main(String[] args) {
+    //     Scanner teclado = new Scanner(System.in);
 
-        System.out.print("¿Cuántos números quieres introducir? ");
-        int cantidad = teclado.nextInt();
+    //     System.out.print("Cuantos numeros quieres introducir ");
+    //     int cantidad = teclado.nextInt();
 
-        //Crear el array y meter los números con push
-        int[] vector = null;
-        for (int i = 0; i < cantidad; i++) {
-            System.out.print("Introduce el número " + (i + 1) + ": ");
-            int num = teclado.nextInt();
-            vector = push(vector, num);
-        }
+    //     //Crear el array y meter los números con push
+    //     int[] vector = null;
+    //     for (int i = 0; i < cantidad; i++) {
+    //         System.out.print("INtroduce el numero " + (i + 1) + ": ");
+    //         int num = teclado.nextInt();
+    //         vector = push(vector, num);
+    //     }
 
-        //Array original
-        System.out.print("Vector original: ");
-        mostrarVector(vector);
+    //     //Array original
+    //     System.out.print("Vector original: ");
+    //     mostrarVector(vector);
 
-        //Mayor a menor
-        ordenarDescendente(vector);
+    //     //Mayor a menor
+    //     ordenarDescendente(vector);
 
-        //Array ordenado
-        System.out.print("Vector ordenado: ");
-        mostrarVector(vector);
-    }
+    //     //Array ordenado
+    //     System.out.print("Vector ordenado: ");
+    //     mostrarVector(vector);
+    // }
 
     
-    //Funcion push añade un valor al array y devuelve el nuevo array
-    public static int[] push(int[] array, int valor) {
-        int longitud;
-        if (array == null) longitud = 0;
-        else longitud = array.length;
-        //Crear nuevo array con +1
-        int[] salida = new int[longitud + 1];
-        //Copiar lo que haya en ese momento
-        for (int i = 0; i < longitud; i++) {
-            salida[i] = array[i];
-        }
-        salida[longitud] = valor;
-        return salida;
-    }
+    // //Funcion push añade un valor al array y devuelve el nuevo array
+    // public static int[] push(int[] array, int valor) {
+    //     int longitud;
+    //     if (array == null) longitud = 0;
+    //     else longitud = array.length;
+    //     //Crear nuevo array con +1
+    //     int[] salida = new int[longitud + 1];
+    //     //Copiar lo que haya en ese momento
+    //     for (int i = 0; i < longitud; i++) {
+    //         salida[i] =array[i];
+    //     }
+    //     salida[longitud] = valor;
+    //     return salida;
+    // }
 
-    // Mostrar el array
-    public static void mostrarVector(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
+    // // Mostrar el array
+    // public static void mostrarVector(int[] array) {
+    //     for (int i = 0; i < array.length; i++) {
+    //         System.out.print(array[i] + " ");
+    //     }
+    //     System.out.println();
+    // }
 
-    // Ordenar de mayor a menor
-    public static void ordenarDescendente(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-            if (array[j] < array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+    // // Ordenar de mayor a menor
+    // public static void ordenarDescendente(int[] array) {
+    //     for (int i = 0; i < array.length - 1; i++) {
+    //         for (int j = 0; j < array.length - 1 - i; j++) {
+    //         if (array[j] < array[j + 1]) {
+    //             int temp =array[j];
+    //             array[j] =array[j + 1];
+    //             array[j + 1] = temp;
+    //         }
+    //         }
+    //     }
+    // }
+
+
+
+    //Ejercicio 3
+    // public static void main(String[] args) {
+    //     int[] vector = {4, 9, 2, 7, 1};
+
+    //     int[] extremos = maxMin(vector);
+    //     System.out.println("Maximo: " + extremos[0]);
+    //     System.out.println("Minimo: " + extremos[1]);
+    // }
+
+    // public static int[] maxMin(int[] array) {
+    //     int max = array[0];
+    //     int min = array[0];
+    //     for (int i = 1; i < array.length; i++) {
+    //         if (array[i] > max) max =array[i];
+    //         if (array[i] < min) min =array[i];
+    //     }
+    //     return new int[]{max, min};
+    // }
+
+
+
+    //Ejercicio 4
+    // public static void main(String[] args) {
+    //     Scanner teclado = new Scanner(System.in);
+
+    //     // Numeros ganadores
+    //     int[] ganadores = {5, 12, 23, 34, 45, 49};
+
+    //     //MI array
+    //     int[] usuario = new int[6];
+    //     System.out.println("Introduce tus 6 numeros de la Bonoloto:");
+    //     for (int i = 0; i < usuario.length; i++) {
+    //         System.out.print("Numero " + (i + 1) + ": ");
+    //         usuario[i] = teclado.nextInt();
+    //     }
+
+    //     System.out.print("Numeros ganadores: ");
+    //     mostrarVector(ganadores);
+    //     System.out.print("Tus numeros: ");
+    //     mostrarVector(usuario);
+
+    //     // Contar aciertos
+    //     int aciertos = contarAciertos(ganadores, usuario);
+    //     System.out.println("Has tenido " + aciertos + " aciertos.");
+    // }
+
+    // // Mostrar array
+    // public static void mostrarVector(int[] array) {
+    //     for (int i = 0; i < array.length; i++) {
+    //         System.out.print(array[i] + " ");
+    //     }
+    //     System.out.println();
+    // }
+
+    // // Contar cuantos numeros coinciden
+    // public static int contarAciertos(int[] ganadores, int[] usuario) {
+    //     int contador = 0;
+    //     for (int i = 0; i < usuario.length; i++) {
+    //         for (int j = 0; j < ganadores.length; j++) {
+    //             if (usuario[i] == ganadores[j]) {
+    //                 contador++;
+    //             }
+    //         }
+    //     }
+    //     return contador;
+    // }
+
+
+
+    //Ejercicio 5
+    // public static void main(String[] args) {
+    //     Scanner teclado = new Scanner(System.in);
+
+    //     //Lista de numeros ganadores
+    //     ArrayList<Integer> ganadores = new ArrayList<>();
+    //     ganadores.add(5);
+    //     ganadores.add(12);
+    //     ganadores.add(23);
+    //     ganadores.add(34);
+    //     ganadores.add(45);
+    //     ganadores.add(49);
+
+    //     //Lista mis numeros
+    //     ArrayList<Integer> usuario = new ArrayList<>();
+    //     System.out.println("Introduce tus 6 números de la Bonoloto:");
+    //     for (int i = 0; i < 6; i++) {
+    //         System.out.print("Número " + (i + 1) + ": ");
+    //         int num = teclado.nextInt();
+    //         usuario.add(num);
+    //     }
+
+    //     System.out.print("Números ganadores: ");
+    //     mostrarLista(ganadores);
+    //     System.out.print("Tus números: ");
+    //     mostrarLista(usuario);
+
+    //     // Contar aciertos
+    //     int aciertos = contarAciertos(ganadores, usuario);
+    //     System.out.println("Has tenido " + aciertos + " aciertos.");
+    // }
+
+    // // Mostrar una lista
+    // public static void mostrarLista(ArrayList<Integer> lista) {
+    //     for (int i = 0; i < lista.size(); i++) {
+    //         System.out.print(lista.get(i) + " ");
+    //     }
+    //     System.out.println();
+    // }
+
+    // // Contar los numeros que coinciden en las listass
+    // public static int contarAciertos(ArrayList<Integer> ganadores, ArrayList<Integer> usuario) {
+    //     int contador = 0;
+    //     for (int i = 0; i < usuario.size(); i++) {
+    //         if (ganadores.contains(usuario.get(i))) {
+    //             contador++;
+    //         }
+    //     }
+    //     return contador;
+    // }
+
+
+
+    //Ejercicio 6
+    // public static void main(String[] args) {
+    //     int[][] matriz1 = {
+    //         {1, 2, 3},
+    //         {4, 5, 6}
+    //     };
+
+    //     int[][] matriz2 = {
+    //         {7, 8, 9},
+    //         {10, 11, 12}
+    //     };
+
+    //     double[][] resultado = calcularColumnas(matriz1, matriz2);
+
+    //     for (int i = 0; i < resultado[0].length; i++) {
+    //         System.out.printf("Columna %d | Suma: %.0f | Producto: %.0f | Media: %.2f%n ",
+    //             i + 1, resultado[0][i], resultado[1][i], resultado[2][i]);
+    //     }
+    // }
+
+    // public static double[][] calcularColumnas(int[][] matriz1, int[][] matriz2) {
+    //     int filas = matriz1.length;
+    //     int columnas = matriz1[0].length;
+
+    //     double[][] salida = new double[3][columnas]; // 3 filas: suma, producto, media
+
+    //     for (int j = 0; j < columnas; j++) {
+    //         int suma = 0;
+    //         int producto = 1;
+    //         for (int i = 0; i < filas; i++) {
+    //             suma += matriz1[i][j];
+    //             producto *= matriz1[i][j];
+    //         }
+    //         for (int i = 0; i < filas; i++) {
+    //             suma += matriz2[i][j];
+    //             producto *= matriz2[i][j];
+    //         }
+    //         salida[0][j] = suma;
+    //         salida[1][j] = producto;
+    //         salida[2][j] = suma / (filas * 2.0); // total de elementos por columna
+    //     }
+
+    //     return salida;
+    // }
+
+
+
+    //Ejercicio 7
+    public static void main(String[] args) {
+        int[][] matriz = new int[5][5];
+        Random rand = new Random();
+
+        // Rellenar matriz con numeros aleatorios entre 0 y 100
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = rand.nextInt(101);
             }
+        }
+
+        System.out.println("Matriz original:");
+        mostrarMatriz(matriz);
+
+        rotarUnaPosicion(matriz);
+
+        System.out.println("Matriz rotada:");
+        mostrarMatriz(matriz);
+    }
+
+    public static void mostrarMatriz(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + "  ");
             }
+            System.out.println(); 
         }
     }
 
+    public static void rotarUnaPosicion(int[][] m) {
+        int temp = m[0][0];
 
+        for (int j = 0; j < 4; j++) {
+            m[0][j] = m[0][j + 1];
+        }
 
+        for (int i = 0; i < 4; i++) {
+            m[i][4] = m[i + 1][4];
+        }
 
+        for (int j = 4; j > 0; j--) {
+            m[4][j] = m[4][j - 1];
+        }
+
+        for (int i = 4; i > 1; i--) {
+            m[i][0] = m[i - 1][0];
+        }
+
+        m[1][0] = temp;
+    
 
 }
+}
+
---
+++ ahorcado/src/App.java
@@ -0,0 +1,111 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Array con (por ejemplo) 5 palabras posibles
+        String[] palabras = {"programacion", "java", "ordenador", "tarea", "ventana"};
+
+        // Elegir una palabra aleatoria
+        String palabra = palabras[(int) (Math.random() * palabras.length)].toLowerCase();
+
+        // Estado del juego: array de caracteres para mostrar (guiones bajos para letras no adivinadas)
+        char[] palabraoculta = new char[palabra.length()];
+        for (int i = 0; i < palabraoculta.length; i++) {
+            palabraoculta[i] = '_';
+        }
+
+        // Letras intentadas
+        Set<Character> intentadas = new HashSet<>();
+        // Letras falladas (mostrar al usuario)
+        Set<Character> falladas = new HashSet<>();
+
+        // Número máximo de fallos antes de perder
+        final int limitefallos = 7;
+        int fallos = 0;
+
+        Scanner teclado = new Scanner(System.in);
+
+        // Bucle principal del juego
+        while (true) {
+            // Estado actual (letras adivinadas o guiones)
+            System.out.print("Palabra: ");
+            // Leer la posición de cada caracter de la palabra
+            for (int i = 0; i < palabraoculta.length; i++) {
+                // 'letra' contiene el caracter que mostramos en cada posicion
+                char letra = palabraoculta[i];
+                System.out.print(letra + " ");
+            }
+            System.out.println();
+
+            // Mostrar letras falladas, número de intentos restantes y letras usadas
+            System.out.println("Letras falladas: " + falladas);
+            System.out.println("Fallos: " + fallos + " / " + limitefallos);
+            System.out.println("Letras usadas: " + intentadas);
+
+            // Comprobar si el jugador ha ganado
+            boolean completo = true;
+            // Recorremos el array con un for clásico para detectar si queda algún guion
+            for (int i = 0; i < palabraoculta.length; i++) {
+                if (palabraoculta[i] == '_') {
+                    completo = false;
+                    break;
+                }
+            }
+            if (completo) {
+                System.out.println("Has adivinado la palabra: " + palabra);
+                break;
+            }
+
+            // Comprobar si ha perdido
+            if (fallos >= limitefallos) {
+                System.out.println("Has perdido. La palabra era: " + palabra);
+                break;
+            }
+
+            // Pedir letra al usuario
+            System.out.print("Introduce una letra: ");
+            String linea = teclado.nextLine();
+            if (linea.isEmpty()) {
+                System.out.println("No has introducido ninguna letra. Intenta de nuevo.");
+                continue;
+            }
+
+            char letraUsada = linea.charAt(0);
+            // Validar que es una letra (a-z)
+            if (letraUsada < 'a' || letraUsada > 'z') {
+                System.out.println("Introduce una letra válida (a-z).\n");
+                continue;
+            }
+
+            // Comprobar si ya se intentó esa letra
+            if (intentadas.contains(letraUsada)) {
+                System.out.println("Ya has intentado la letra '" + letraUsada + "'. Elige otra.");
+                continue;
+            }
+
+            // Marcar la letra como intentada
+            intentadas.add(letraUsada);
+
+            // Comprobar si la letra está en la palabra y actualizar el estado
+            boolean acierto = false;
+            for (int i = 0; i < palabra.length(); i++) {
+                if (palabra.charAt(i) == letraUsada) {
+                    palabraoculta[i] = letraUsada; // Revelar la letra en todas sus posiciones
+                    acierto = true;
+                }
+            }
+
+            if (acierto) {
+                System.out.println("¡Bien! La letra '" + letraUsada + "' está en la palabra.\n");
+            } else {
+                System.out.println("La letra '" + letraUsada + "' NO está en la palabra.\n");
+                fallos++;            // Aumentar contador de fallos
+                falladas.add(letraUsada); // Añadir a la lista de falladas para mostrar
+            }
+        }
+
+    }
+}
