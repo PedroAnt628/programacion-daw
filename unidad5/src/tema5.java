accdcf1e686b92814a26b13eb80b6cbfe682684d
PedroAnt628 <pemorecarr@gmail.com>
2025-12-16 12:46:47 +0100

    .

--- unidad5/src/tema5.java
+++ unidad5/src/tema5.java
@@ -1,34 +1,24 @@
-package SegundoTrimes;
+package unidad5.src;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-public class unidad5 {
-    
+public class tema5 {
     public static void main(String[] args) {
-        //System.out.println("Directorio actual: " + new File(".").getAbsolutePath());
-        // try {
-        // File fichero = new File("heroes.json");
-        // System.out.println(fichero.exists());
-        // System.out.println("Nombre: " + fichero.getName());
-        // System.out.println("Permiso de lectura: " + fichero.canRead());
-        // System.out.println("Tamaño: " + fichero.length());
-        // }catch (Exception e){
-        //     System.out.println(e.getMessage());
-        // }
+        System.out.println(new File(".").getAbsolutePath());
 
         File archivo = null;
         FileReader reader = null;
         BufferedReader buffer = null;
 
         try {
-            archivo = new File("heroes.json");
+            archivo = new File("numeros.txt");
             reader = new FileReader(archivo);
             buffer = new BufferedReader(reader);
 
             String linea;
-            while ((linea=buffer.readLine()) != null){
+            while ( (linea=buffer.readLine()) != null) {
                 System.out.println(linea);
             }
         }
@@ -43,6 +33,5 @@ public class unidad5 {
                 e2.printStackTrace();
             }
         }
-
     }
 }
